674facb9ec88df1db5dea55cc51a024c8f0549c3
iamosl <osl.lavangad@gmail.com>
2021-12-11 23:07:03 +0530

    Fixed delete user. and also made changes in database

--- geartracker-backend/src/main/java/com/geartracker/geartracker_backend/UserRepository.java
+++ geartracker-backend/src/main/java/com/geartracker/geartracker_backend/UserRepository.java
@@ -313,11 +313,19 @@ public class UserRepository {
 		return getUserById(newU.getId()); 
 	}
 	
-	public String deleteUser(String id) {
-		String sqlQuery_delete = "DELETE from user WHERE user_id = '" + id +"'";
+	public String deleteUser(User u,String id) {
 		try {
 			Statement st = conn.createStatement();
-			st.executeUpdate(sqlQuery_delete);
+			String sqlQuery = "";
+			if(u.getRoles().contains("student"))
+			{
+				sqlQuery= "DELETE from student WHERE surrogate_id = "+ u.getStudent();
+			}
+			else
+			{
+				sqlQuery= "DELETE from user WHERE user_id = '" + id +"'";
+			}
+			st.executeUpdate(sqlQuery);
 			return Constants.SUCCESS_STATUS;
 			
 		} catch(Exception exc) {
@@ -325,6 +333,7 @@ public class UserRepository {
 		}
 		return Constants.FAILURE_STATUS;
 	}
+
 
 }
 
