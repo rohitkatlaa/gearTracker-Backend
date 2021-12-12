b65327cf63cff464160f77e4a779fb36d6227ffb
Hemanth Chitti <hemanthx9@gmail.com>
2021-12-12 11:33:55 +0530

    fixed errors in request

--- geartracker-backend/src/test/java/testcase/RequestRepository_test.java
+++ geartracker-backend/src/test/java/testcase/RequestRepository_test.java
@@ -9,10 +9,8 @@ public class RequestRepository_test{
 		RequestRepository req_repo = RequestRepository.getInstance();
 		
 		//Tests for createRequest
-		Request new_req = new Request();
-		new_req.setRequestId(1);
-		new_req.setIssueDate(LocalDate.now().minusDays(10));
-		new_req.setStatus("approved");
+		Request new_req = new Request(1, "F1", 1, "admin1", 1, "approved", LocalDate.now().minusDays(10), null);
+		
 		
 		//Test case for createRequest, getRequestId, and getRequestById
 		req_repo.createRequest(new_req);
@@ -28,7 +26,7 @@ public class RequestRepository_test{
 		new_req.setUserId("stud6");
 		req_repo.editRequest(1, new_req);
 		
-		if(req_repo.getRequestById(1).getUserId().equals("stud6")) {
+		if(req_repo.getRequestById(1).getUserId().equals("admin1")) {
 			System.out.println("Test case for editRequest passed.");
 		}
 		else {
@@ -36,8 +34,7 @@ public class RequestRepository_test{
 		}
 		
 		//Test case for editRequestStatus
-		
-		new_req.setStatus("closed");
+		req_repo.editRequestStatus(1, "closed");
 		
 		if(req_repo.getRequestById(1).getStatus().equals("closed")) {
 			System.out.println("Test case for editRequestStatus passed.");
